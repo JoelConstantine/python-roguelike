4c03de581504baad980a64ec45efd261e1b132e0
Joel <joel@joelconstantine.com>
2023-02-06 10:43:07 -0600

    Added saving and loading and a menu screen, as per part 10

--- components/fighter.py
+++ components/fighter.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import color
 from components.base_component import BaseComponent
-from input_handlers import GameOverEventHandler
+
 from render_order import RenderOrder
 
 if TYPE_CHECKING:
@@ -32,7 +32,6 @@ class Fighter(BaseComponent):
     def die(self) -> None:
         if self.engine.player is self.parent:
             death_message = "You died!"
-            self.engine.event_handler = GameOverEventHandler(self.engine)
             death_color = color.player_die
         else:
             death_message = f"{self.parent.name} is dead!"
--- main.py
+++ main.py
@@ -1,58 +1,32 @@
 """All of this is based on the TCOD roguelike tutorial (2020) found at http://rogueliketutorials.com"""
 
 
-import copy
 import traceback
 
 import tcod
 
 import color
-from engine import Engine
-import entity_factories
-from procgen import generate_dungeon
+import exceptions
+import input_handlers
+import setup_game
 
-def main():
+def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
+    """If the current event handler has an active Engine then save it"""
+    if isinstance(handler, input_handlers.EventHandler):
+        handler.engine.save_as(filename)
+        print("Game saved")
+
+def main() -> None:
     # Setup initial variables for the game
     screen_width = 80
     screen_height = 50
-
-    map_width = 80
-    map_height = 43
-
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-    max_monsters_per_room = 2
-    max_items_per_room = 2
 
     # Load the provided tileset
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-
-    # Create the player
-    player = copy.deepcopy(entity_factories.player)
-
-    # Initialize the engine, passing in the player
-    engine = Engine(player=player)
-
-    # Create the dungeon map for the engine
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms, 
-        room_min_size=room_min_size, 
-        room_max_size=room_max_size, 
-        map_width=map_width, 
-        map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
-        engine=engine
-    )
-
-    engine.update_fov()
-
-    engine.message_log.add_message(
-        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
-    )
+    
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     # Create a new tcod tutorial with our settings and tileset, begin the gameloop
     with tcod.context.new_terminal(
@@ -63,29 +37,39 @@ def main():
         vsync=True
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
-        while True:
-            """
-            This is the basic game engine loop
-            
-            First the console is cleared,
-            then the engine's event_handler render its current state
-            
-            Afterwards, the engine then waits for and responds to player input
-            before updating the loop
-            """
-            
-            root_console.clear()
-            engine.event_handler.on_render(console=root_console)
-            context.present(root_console)
+        try:
+            while True:
+                """
+                This is the basic game engine loop
+                
+                First the console is cleared,
+                then the engine's event_handler render its current state
+                
+                Afterwards, the engine then waits for and responds to player input
+                before updating the loop
+                """
+                
+                root_console.clear()
+                handler.on_render(console=root_console)
+                context.present(root_console)
 
-            try:
-                for event in tcod.event.wait():
-                    context.convert_event(event)
-                    engine.event_handler.handle_events(event)
-            except Exception: # Handle exceptions in game
-                traceback.print_exc() # Print erro to stderr
-                # The print the error to the message log
-                engine.message_log.add_message(traceback.format_exc(), color.error)
+                try:
+                    for event in tcod.event.wait():
+                        context.convert_event(event)
+                        handler = handler.handle_events(event)
+                except Exception: # Handle exceptions in game
+                    traceback.print_exc() # Print erro to stderr
+                    # Then print the error to the message log
+                    if isinstance(handler, input_handlers.EventHandler):
+                        handler.engine.message_log.add_message(traceback.format_exc(), color.error)
+        except exceptions.QuitWithoutSaving:
+            raise
+        except SystemExit: # Save and quit
+            save_game(handler, "savegame.sav")
+            raise
+        except BaseException:
+            save_game(handler, "savegame.sav")
+            raise
 
 
 if __name__ == "__main__":
